78604e33adc075f87362807ec2c6b99e32b2543d
sans-connaissance <davemalicke@gmail.com>
2021-09-01 20:47:26 -0400

    Initial Commit

---
+++ Car Dealer App Tutorial/Car_Dealer_App_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_Dealer_App_TutorialApp.swift
+//  Car Dealer App Tutorial
+//
+//  Created by David Malicke on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Car_Dealer_App_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
